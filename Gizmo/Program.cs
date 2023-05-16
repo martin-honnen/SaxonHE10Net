c97262053ee888cb39f73c714d2c03e720d2dce5
Martin Honnen <martin.honnen@gmx.de>
2023-05-16 18:05:11 +0200

    Projektdateien hinzufügen.

---
+++ Gizmo/Program.cs
@@ -0,0 +1,13 @@
+﻿using com.sun.xml.@internal.rngom.parse.host;
+using net.sf.saxon;
+
+namespace Saxon.Cmd
+{
+    public class DotNetGizmo : Gizmo
+    {
+        public static void Main(string[] args)
+        {
+            main(args);
+        }
+    }
+}
